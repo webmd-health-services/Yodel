637b91db65af5781ba7e8b00dbd778d2331ead58
Aaron Jensen <ajensen@webmd.net>
2023-05-02 15:30:04 -0700

    Fixed: importing fails when module files merged together.

--- Yodel/Yodel.psm1
+++ Yodel/Yodel.psm1
@@ -2,15 +2,15 @@
 #Requires -Version 5.1
 Set-StrictMode -Version 'Latest'
 
+# Functions should use $script:moduleRoot as the relative root from which to find
+# things. A published module has its function appended to this file, while a
+# module in development has its functions in the Functions directory.
+$script:moduleRoot = $PSScriptRoot
+
 enum Yodel_MsSql_QueryKeyword
 {
     Default = 1
 }
-
-# Functions should use $moduleRoot as the relative root from which to find
-# things. A published module has its function appended to this file, while a
-# module in development has its functions in the Functions directory.
-$moduleRoot = $PSScriptRoot
 
 # Store each of your module's functions in its own file in the Functions
 # directory. On the build server, your module's functions will be appended to
@@ -18,13 +18,16 @@ $moduleRoot = $PSScriptRoot
 # developers to work on a module without having to build it first. Grab all the
 # functions that are in their own files.
 $functionsPath = & {
-    Join-Path -Path $moduleRoot -ChildPath 'Functions\*.ps1'
-    Join-Path -Path $moduleRoot -ChildPath 'Functions\MsSql\*.ps1'
+    Join-Path -Path $script:moduleRoot -ChildPath 'Functions\*.ps1'
+    Join-Path -Path $script:moduleRoot -ChildPath 'Functions\MsSql\*.ps1'
 }
-if( (Test-Path -Path $functionsPath) )
+foreach( $functionPath in (Get-Item $functionsPath) )
 {
-    foreach( $functionPath in (Get-Item $functionsPath) )
+    if ( -not (Test-Path -Path $functionPath))
     {
-        . $functionPath.FullName
+        continue
     }
+
+
+    . (Get-Item -Path $functionPath).FullName
 }
